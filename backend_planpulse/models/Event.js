0256762950dd0702b026e268a49dac3a89239e67
Alec Zytkowski <aleczytkowski@gmail.com>
2023-10-10 20:20:01 -0400

    Added/Removed Comments for User and Event Schema

--- backend_planpulse/models/Event.js
+++ backend_planpulse/models/Event.js
@@ -6,7 +6,6 @@ const eventSchema = new mongoose.Schema({
   startDateTime: { type: Date, required: true },
   endDateTime: { type: Date, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  // Add more fields as needed for event data
 });
 
 const Event = mongoose.model('Event', eventSchema);
