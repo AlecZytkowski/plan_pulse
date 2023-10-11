f590dc354b21b1d9b0bd696acad10e3697b418a9
Alec Zytkowski <aleczytkowski@gmail.com>
2023-10-10 21:32:52 -0400

    Redesigned Dashboard for improved user experience.

--- plan_pulse/src/Pages/Dashboard/Dashboard.jsx
+++ plan_pulse/src/Pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,32 @@
 import './Dashboard.css';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import { useState } from 'react';
+
 import { UserProfile } from './UserProfile';
-import { CalenderView } from './CalenderView';
+import { CalendarView } from './CalendarView';
 import { CreateEvent } from './CreateEvent';
 
 export const Dashboard = () => {
-    return (
-      <div>
-        <h1>Welcome to the Dashboard</h1>
-        <UserProfile />
-        <CreateEvent />
-        <CalenderView />
+  const [selectedOption, setSelectedOption] = useState('profile');
+
+  return (
+    <div className="dashboard-container">
+      <div className="header-logo">
+        <img src={process.env.PUBLIC_URL + '/PlanPulseLogo.png'} alt="Plan Pulse Logo" className="logo-image" />
       </div>
-    );
-  };
+      <nav className="navbar">
+        <ul>
+          <li onClick={() => setSelectedOption('calendar')}>Calendar</li>
+          <li onClick={() => setSelectedOption('create-event')}>Create Event</li>
+          <li onClick={() => setSelectedOption('profile')}>User Profile</li>
+        </ul>
+      </nav>
+
+      <div className="dashboard-content">
+        {selectedOption === 'calendar' && <CalendarView />}
+        {selectedOption === 'create-event' && <CreateEvent />}
+        {selectedOption === 'profile' && <UserProfile />}
+      </div>
+    </div>
+  );
+};
