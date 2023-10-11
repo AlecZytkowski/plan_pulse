c5aa850d1e84cad5064cc60e15d17a8033f2acf7
Alec Zytkowski <aleczytkowski@gmail.com>
2023-10-10 21:33:56 -0400

    Setup API call to backend to retrieve user profile data.

--- plan_pulse/src/Pages/Dashboard/UserProfile.jsx
+++ plan_pulse/src/Pages/Dashboard/UserProfile.jsx
@@ -3,9 +3,32 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const UserProfile = () => {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+    
+    axios.get('http://localhost:5000/api/users/profile', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+    .then((response) => {
+      setUserData(response.data.user);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, []);
+
     return (
         <div>
-            <h1>User Profile</h1>
+          <h1>User Profile</h1>
+          {userData && (
+            <div>
+              <p>Username: {userData.username}</p>
+              <p>Email: {userData.email}</p>
+            </div>
+          )}
         </div>
-    )
+      )
 }
