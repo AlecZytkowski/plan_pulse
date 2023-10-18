12c7216d40a4fad0cbd73b6a63f21756ca6b9550
Alec Zytkowski <aleczytkowski@gmail.com>
2023-10-17 21:50:17 -0400

    Added userImageUrl to Schema to track user's profile images.

--- backend_planpulse/models/User.js
+++ backend_planpulse/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
+    userImageUrl: {type: String, required: false},
     password: {type: String, required: true},
     //Should this be true?
     token: {type: String, required: false}
