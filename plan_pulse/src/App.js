a3920fc93006c9287e66f0084eb861e0f75008a8
Alec Zytkowski <aleczytkowski@gmail.com>
2023-10-28 14:56:55 -0400

    Added Authentication state to track when a user is logged in to prevent unauthorized access.

--- plan_pulse/src/App.js
+++ plan_pulse/src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import { WelcomePage } from './Pages/WelcomePage';
 import { RegistrationForm } from './Pages/RegistrationForm';
 import { LoginForm } from './Pages/LoginForm';
 import { Dashboard } from './Pages/Dashboard/Dashboard';
 
+import { ProtectedRoute } from './ProtectedRoute';
 
 
 function App() {
   const [isExistingUser, setIsExistingUser] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
 
   const toggleForm = () => {
     setIsExistingUser((prevState) => !prevState);
   };
+
+  const checkAuthentication = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthenticated(true);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
 
 
   return (
@@ -22,9 +36,9 @@ function App() {
       <Router>
         <Routes>
             <Route path="/" element={<WelcomePage />}></Route>
-            <Route path="/login" element={<LoginForm toggleForm={toggleForm} />}></Route>
+            <Route path="/login" element={<LoginForm toggleForm={toggleForm} setAuthenticated={setAuthenticated} />} />
             <Route path="/register" element={<RegistrationForm toggleForm={toggleForm} />}></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/dashboard" element={<ProtectedRoute authenticated={authenticated} redirectTo="/login" element={<Dashboard setAuthenticated={setAuthenticated} />} /> }/>
         </Routes>
       </Router>
     </div>
